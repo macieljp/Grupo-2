56537227b61bf76ded331915452ce41642a414ea
Tayly <usaf_tayly@hotmail.com>
2013-08-20 20:40:08 -0300

    Criação HibernateConfiguration

--- SysDistribuidora/src/br/edu/utfpr/cm/pi/api/HibernateConfiguration.java
+++ SysDistribuidora/src/br/edu/utfpr/cm/pi/api/HibernateConfiguration.java
@@ -4,10 +4,122 @@
  */
 package br.edu.utfpr.cm.pi.api;
 
+import br.edu.utfpr.cm.pi.entidades.Cliente;
+import br.edu.utfpr.cm.pi.entidades.Endereco;
+import br.edu.utfpr.cm.pi.entidades.Fornecedor;
+import br.edu.utfpr.cm.pi.entidades.Produto;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.mapping.PersistentClass;
+import org.hibernate.tool.hbm2ddl.SchemaExport;
+
 /**
  *
  * @author TAYLY
  */
-class HibernateConfiguration {
-    
+public class HibernateConfiguration {
+    // configuracÃµes somente uma vez 
+    // build  valida somente uma vez
+    // Session uma sessÃ£o para cada transaÃ§Ã£o ou um conjunto de transaÃ§Ãµes
+
+    private static AnnotationConfiguration cfg;
+    private static SessionFactory sessionFactory;
+    private static String user = "root";
+    private static String pass = "aluno";
+    private static String base = "asstec";
+    private static String host = "localhost:3306";
+
+    public static Session openConnect() {
+        if (cfg == null) {
+            cfg = new AnnotationConfiguration();
+            cfg.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
+            cfg.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
+            cfg.setProperty("hibernate.connection.username", user);
+            cfg.setProperty("hibernate.connection.password", pass);
+            cfg.setProperty("hibernate.connection.url", "jdbc:mysql://" + host + "/" + base);
+            cfg.setProperty("hibernate.show_sql", "true");
+            cfg.setProperty("hibernate.connection.autocommit", "true");
+
+            cfg.addAnnotatedClass(Cliente.class);
+            cfg.addAnnotatedClass(Endereco.class);
+            cfg.addAnnotatedClass(Fornecedor.class);
+            cfg.addAnnotatedClass(Produto.class);
+        //    cfg.addAnnotatedClass(Produto.class);
+          //  cfg.addAnnotatedClass(UnidadeMedida.class);
+
+
+
+            sessionFactory = cfg.buildSessionFactory();
+        }
+        return sessionFactory.openSession();
+    }
+
+    public static AnnotationConfiguration getCfg() {
+        return cfg;
+    }
+
+    public static void setCfg(AnnotationConfiguration cfg) {
+        HibernateConfiguration.cfg = cfg;
+    }
+
+    public static String getBase() {
+        return base;
+    }
+
+    public static void setBase(String base) {
+        HibernateConfiguration.base = base;
+    }
+
+    public static String getPass() {
+        return pass;
+    }
+
+    public static void setPass(String pass) {
+        HibernateConfiguration.pass = pass;
+    }
+
+    public static String getUser() {
+        return user;
+    }
+
+    public static void setUser(String user) {
+        HibernateConfiguration.user = user;
+    }
+
+    public static String getHost() {
+        return host;
+    }
+
+    public static void setHost(String host) {
+        HibernateConfiguration.host = host;
+    }
+
+    public static void criarSchema() {
+        openConnect().close();
+        org.hibernate.tool.hbm2ddl.SchemaExport schemaEx = new SchemaExport(cfg);
+        schemaEx.create(true, true);
+    }
+
+    public static List<Object> runHQLQuery(String hql) {
+        Session session = TransactionManager.getCurrentSession();
+        return session.createQuery(hql).list();
+    }
+
+    public static ArrayList<Class> getEntityClasses() {
+        ArrayList<Class> classes = new ArrayList<Class>();
+        if (cfg == null) {
+            openConnect();
+        }
+        Iterator i = cfg.getClassMappings();
+        while (i.hasNext()) {
+            classes.add(((PersistentClass) i.next()).getMappedClass());
+        }
+        return classes;
+    }
 }
+
+    
