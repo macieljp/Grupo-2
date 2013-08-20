4c0ceb647c68d99f9e48a557613c478a34f38bf9
Tayly <usaf_tayly@hotmail.com>
2013-08-20 11:42:56 -0300

    Criação da classe "DaoManager" e "Operador" no pacote api

---
+++ SysDistribuidora/src/br/edu/utfpr/cm/pi/api/Operator.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.utfpr.cm.pi.api;
+
+/**
+ *
+ * @author TAYLY
+ */
+class Operator {
+    
+    IS_NULL, 
+    IS_NOT_NULL, 
+    LIKE, 
+    EQUAL, 
+    MORE_THAN, 
+    MORE_THAN_EQUAL, 
+    LESS_THAN, 
+    LESS_THAN_EQUAL;
+    
+}
