bfb85fdbec7059eeb8aac05858e9747f00db00a8
Tayly <usaf_tayly@hotmail.com>
2013-08-21 12:25:40 -0300

    Criação das entidades Telefone

--- SysDistribuidora/src/br/edu/utfpr/cm/pi/entidades/GrupodeProduto.java
+++ SysDistribuidora/src/br/edu/utfpr/cm/pi/entidades/GrupodeProduto.java
@@ -4,10 +4,101 @@
  */
 package br.edu.utfpr.cm.pi.entidades;
 
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import org.hibernate.annotations.Entity;
+
 /**
  *
  * @author TAYLY
  */
 public class GrupodeProduto {
     
+    /**
+     *
+     */
+    @Entity
+   
+    public class GrupodeProduto implements  Serializable {
+
+    @Id    
+    @GeneratedValue(strategy=GenerationType.AUTO)  
+   
+    @OneToOne
+    private int id;
+    
+    @Column
+    private String descricao;
+    
+    @Column
+    private int unidade;
+    
+    @OneToMany    
+    private String produto;
+    
+    @OneToMany
+    private String classificacao;
+
+        public int getId() {
+            return id;
+        }
+
+        public String getDescricao() {
+            return descricao;
+        }
+
+        public int getUnidade() {
+            return unidade;
+        }
+
+        public String getProduto() {
+            return produto;
+        }
+
+        public String getClassificacao() {
+            return classificacao;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public void setDescricao(String descricao) {
+            this.descricao = descricao;
+        }
+
+        public void setUnidade(int unidade) {
+            this.unidade = unidade;
+        }
+
+        public void setProduto(String produto) {
+            this.produto = produto;
+        }
+
+        public void setClassificacao(String classificacao) {
+            this.classificacao = classificacao;
+        }
+    
+
+   
+    
+    
+    
+    
+    @Override
+ 
+  public String toString(){
+ 
+    return "Pessoa {" + "id: " + id + 
+                    "Descrição: " + descricao+
+                    "Unidade:  " + unidade+
+                    "Produto: "+produto+
+                    "Classificação: " +classificacao+ "}";
+        }
+    }
 }
--- SysDistribuidora/src/br/edu/utfpr/cm/pi/entidades/Telefone.java
+++ SysDistribuidora/src/br/edu/utfpr/cm/pi/entidades/Telefone.java
@@ -4,10 +4,68 @@
  */
 package br.edu.utfpr.cm.pi.entidades;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
+
 /**
  *
  * @author TAYLY
  */
 public class Telefone {
+     @Id    
+    @GeneratedValue(strategy=GenerationType.AUTO)  
+    
+     @OneToOne
+     private int id;
+    
+     @ManyToMany
+    private int numero;
+    
+     @OneToOne
+    private int ddd;
+
+    
+     /**
+     *
+     * @param id
+     * @param numero
+     * @param ddd
+     */
+    //public Telefone (int id ,int numero,int ddd);
+     
+     public int getId() {
+        return id;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public int getDdd() {
+        return ddd;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public void setDdd(int ddd) {
+        this.ddd = ddd;
+    }
+    
+     @Override
+    public String toString() {
+        return "Endereco{" + "id=" + id +
+                ",numero=" + numero + 
+                ", ddd=" + ddd +'}';
+    }
     
 }
