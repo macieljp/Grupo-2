acb70258952044f7c891eab36660bfe51e835ab9
Paulo Azevedo <paulovulture@hotmail.com>
2013-08-21 14:13:55 -0300

    Com as anotaçoes corretas e criando a base certinho.

--- SysDistribuidora/src/br/edu/utfpr/cm/pi/entidades/GrupodeProduto.java
+++ SysDistribuidora/src/br/edu/utfpr/cm/pi/entidades/GrupodeProduto.java
@@ -11,88 +11,78 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
-import org.hibernate.annotations.Entity;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 
 /**
  *
  * @author TAYLY
  */
+/**
+ *
+ */
+@Entity
+public class GrupodeProduto implements Serializable {
 
-    
-    /**
-     *
-     */
-    @Entity
-   
-    public class GrupodeProduto implements  Serializable {
-
-    @Id    
-    @GeneratedValue(strategy=GenerationType.AUTO)  
-   
-    @OneToOne
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
-    
     @Column
     private String descricao;
-    
     @Column
     private int unidade;
     
-    @OneToMany    
+    @ManyToOne
     private Produto produto;
-    
-  
     private String classificacao;
 
-        public int getId() {
-            return id;
-        }
-
-        public String getDescricao() {
-            return descricao;
-        }
-
-        public int getUnidade() {
-            return unidade;
-        }
-
-        public Produto getProduto() {
-            return produto;
-        }
-
-        public String getClassificacao() {
-            return classificacao;
-        }
-
-        public void setId(int id) {
-            this.id = id;
-        }
-
-        public void setDescricao(String descricao) {
-            this.descricao = descricao;
-        }
-
-        public void setUnidade(int unidade) {
-            this.unidade = unidade;
-        }
-
-        public void setProduto(Produto produto) {
-            this.produto = produto;
-        }
-
-        public void setClassificacao(String classificacao) {
-            this.classificacao = classificacao;
-        }
-    
-    
-    @Override
- 
-  public String toString(){
- 
-    return "Pessoa {" + "id: " + id + 
-                    "Descrição: " + descricao+
-                    "Unidade:  " + unidade+
-                    "Produto: "+produto+
-                    "Classificação: " +classificacao+ "}";
-        }
+    public int getId() {
+        return id;
     }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public int getUnidade() {
+        return unidade;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public String getClassificacao() {
+        return classificacao;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setUnidade(int unidade) {
+        this.unidade = unidade;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public void setClassificacao(String classificacao) {
+        this.classificacao = classificacao;
+    }
+
+    @Override
+    public String toString() {
+
+        return "Pessoa {" + "id: " + id
+                + "Descrição: " + descricao
+                + "Unidade:  " + unidade
+                + "Produto: " + produto
+                + "Classificação: " + classificacao + "}";
+    }
+}
