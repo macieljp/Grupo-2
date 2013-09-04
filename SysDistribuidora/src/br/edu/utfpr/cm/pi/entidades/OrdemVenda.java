44b7bf1d1e929237034910d9b082e793b326b081
Tayly <usaf_tayly@hotmail.com>
2013-09-04 19:51:28 -0300

    #2 criação entidade OrdemVenda

--- SysDistribuidora/src/br/edu/utfpr/cm/pi/entidades/OrdemVenda.java
+++ SysDistribuidora/src/br/edu/utfpr/cm/pi/entidades/OrdemVenda.java
@@ -9,6 +9,7 @@ import java.util.Calendar;
 import javax.persistence.Basic;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -22,6 +23,8 @@ import org.hibernate.annotations.Cascade;
  *
  * @author TAYLY
  */
+@Entity
+
 public class OrdemVenda implements Serializable {
 
     @Id
@@ -69,4 +72,238 @@ public class OrdemVenda implements Serializable {
     private String agenciaCheque;
     private String contaCheque;
     
+    public String getNomeDoCheque() {
+        return nomeDoCheque;
+    }
+
+    public void setNomeDoCheque(String nomeDoCheque) {
+        this.nomeDoCheque = nomeDoCheque;
+    }
+
+    public String getCnpjDoCheque() {
+        return cnpjDoCheque;
+    }
+
+    public void setCnpjDoCheque(String cnpjDoCheque) {
+        this.cnpjDoCheque = cnpjDoCheque;
+    }
+
+    public String getValorDoCheque() {
+        return valorDoCheque;
+    }
+
+    public void setValorDoCheque(String valorDoCheque) {
+        this.valorDoCheque = valorDoCheque;
+    }
+
+    public String getDataCheque() {
+        return dataCheque;
+    }
+
+    public void setDataCheque(String dataCheque) {
+        this.dataCheque = dataCheque;
+    }
+
+    public String getAgenciaCheque() {
+        return agenciaCheque;
+    }
+
+    public void setAgenciaCheque(String agenciaCheque) {
+        this.agenciaCheque = agenciaCheque;
+    }
+
+    public String getContaCheque() {
+        return contaCheque;
+    }
+
+    public void setContaCheque(String contaCheque) {
+        this.contaCheque = contaCheque;
+    }
+    
+    
+
+    public OrdemVenda() {
+    }
+
+    public double getJuros() {
+        return juros;
+    }
+
+    public void setJuros(double juros) {
+        this.juros = juros;
+    }
+
+    public String getValorPorExtenso() {
+        return valorPorExtenso;
+    }
+
+    public void setValorPorExtenso(String valorPorExtenso) {
+        this.valorPorExtenso = valorPorExtenso;
+    }
+
+    public String getBanco() {
+        return banco;
+    }
+
+    public void setBanco(String banco) {
+        this.banco = banco;
+    }
+
+    public String getTipoCheque() {
+        return tipoCheque;
+    }
+
+    public void setTipoCheque(String tipoCheque) {
+        this.tipoCheque = tipoCheque;
+    }
+
+    public Funcionario getFuncionario() {
+        return funcionario;
+    }
+
+    public void setFuncionario(Funcionario funcionario) {
+        this.funcionario = funcionario;
+    }
+
+    public Integer getParcelasRestantes() {
+        return parcelasRestantes;
+    }
+
+    public void setParcelasRestantes(Integer parcelasRestantes) {
+        this.parcelasRestantes = parcelasRestantes;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+ 
+
+    public Calendar getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public void setDataVencimento(Calendar dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+
+    public Pessoa getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Pessoa cliente) {
+        this.cliente = cliente;
+    }
+
+   
+
+    public Double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(Double valor) {
+        this.valorTotal = valor;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Integer getParcelas() {
+        return parcelas;
+    }
+
+    public void setParcelas(Integer parcelas) {
+        this.parcelas = parcelas;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getItem1() {
+        return item1;
+    }
+
+    public void setItem1(String item1) {
+        this.item1 = item1;
+    }
+
+    public String getItem2() {
+        return item2;
+    }
+
+    public void setItem2(String item2) {
+        this.item2 = item2;
+    }
+
+    public String getItem3() {
+        return item3;
+    }
+
+    public void setItem3(String item3) {
+        this.item3 = item3;
+    }
+
+    public String getItem4() {
+        return item4;
+    }
+
+    public void setItem4(String item4) {
+        this.item4 = item4;
+    }
+
+    public Double getValor1() {
+        return valor1;
+    }
+
+    public void setValor1(Double valor1) {
+        this.valor1 = valor1;
+    }
+
+    public Double getValor2() {
+        return valor2;
+    }
+
+    public void setValor2(Double valor2) {
+        this.valor2 = valor2;
+    }
+
+    public Double getValor3() {
+        return valor3;
+    }
+
+    public void setValor3(Double valor3) {
+        this.valor3 = valor3;
+    }
+
+    public Double getValor4() {
+        return valor4;
+    }
+
+    public void setValor4(Double valor4) {
+        this.valor4 = valor4;
+    }
+
+    @Override
+    public String toString() {
+        return "OrdemVenda{" + "id=" + id + ", dataVencimento=" + dataVencimento + ", cliente=" + cliente + ", funcionario=" + funcionario + ", item1=" + item1 + ", item2=" + item2 + ", item3=" + item3 + ", item4=" + item4 + ", valor1=" + valor1 + ", valor2=" + valor2 + ", valor3=" + valor3 + ", valor4=" + valor4 + ", valorTotal=" + valorTotal + ", status=" + status + ", parcelas=" + parcelas + ", parcelasRestantes=" + parcelasRestantes + ", endereco=" + endereco + ", banco=" + banco + ", tipoCheque=" + tipoCheque + ", juros=" + juros + ", valorPorExtenso=" + valorPorExtenso + '}';
+    }
+
+ 
 }
+
+    
+
