4dd90841acfa5e7436fd58f34d0823e2311f8176
Tayly <usaf_tayly@hotmail.com>
2013-09-04 12:04:47 -0300

    Entidade Fisico

--- SysDistribuidora/src/br/edu/utfpr/cm/pi/entidades/Fisico.java
+++ SysDistribuidora/src/br/edu/utfpr/cm/pi/entidades/Fisico.java
@@ -4,10 +4,123 @@
  */
 package br.edu.utfpr.cm.pi.entidades;
 
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 /**
  *
  * @author TAYLY
  */
-class Fisico {
-    
+@Entity
+public class Fisico extends Pessoa implements Serializable {
+
+    public Fisico() {
+    }
+
+    public Fisico(String nome, String sobrenome, String rg, String cpf, Calendar nascimento, Integer id, Endereco endereco, String email, Calendar dataCadastro) {
+        super(id, endereco, email, dataCadastro);
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.rg = rg;
+        this.cpf = cpf;
+        this.nascimento = nascimento;
+    }
+    @Id
+    private String nome;
+    private String sobrenome;
+    private String rg;
+    private String cpf;
+    @Temporal(TemporalType.DATE)
+    private Calendar nascimento;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Calendar getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(Calendar nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 71 * hash + Objects.hashCode(this.nome);
+        hash = 71 * hash + Objects.hashCode(this.sobrenome);
+        hash = 71 * hash + Objects.hashCode(this.rg);
+        hash = 71 * hash + Objects.hashCode(this.cpf);
+        hash = 71 * hash + Objects.hashCode(this.nascimento);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Fisico other = (Fisico) obj;
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.sobrenome, other.sobrenome)) {
+            return false;
+        }
+        if (!Objects.equals(this.rg, other.rg)) {
+            return false;
+        }
+        if (!Objects.equals(this.cpf, other.cpf)) {
+            return false;
+        }
+        if (!Objects.equals(this.nascimento, other.nascimento)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Fisico{" + "nome=" + nome + ", sobrenome=" + sobrenome + ", rg=" + rg + ", cpf=" + cpf + ", nascimento=" + nascimento + '}';
+    }
 }
+
+    
+
