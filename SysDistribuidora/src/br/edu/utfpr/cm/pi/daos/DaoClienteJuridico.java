777ce8829f3702995801cceb4d5209b74d3527fd
Tayly <usaf_tayly@hotmail.com>
2013-09-04 20:04:15 -0300

    #3 ClienteJuridico

--- SysDistribuidora/src/br/edu/utfpr/cm/pi/daos/DaoClienteJuridico.java
+++ SysDistribuidora/src/br/edu/utfpr/cm/pi/daos/DaoClienteJuridico.java
@@ -43,6 +43,7 @@ public class DaoClienteJuridico extends DaoGenerics<ClienteJuridico>{
                     + " where cnpj LIKE '"
                     + cnpj + "%'");
             lista = query.list();
+            lista = query.list();
         }
         return lista;
     }
