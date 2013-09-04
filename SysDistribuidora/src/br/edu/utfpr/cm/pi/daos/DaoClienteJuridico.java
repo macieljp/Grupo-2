58fff0140219636b1443a713ddd3c17206f630bc
Tayly <usaf_tayly@hotmail.com>
2013-09-04 19:59:05 -0300

    #1 ClienteJuridico

--- SysDistribuidora/src/br/edu/utfpr/cm/pi/daos/DaoClienteJuridico.java
+++ SysDistribuidora/src/br/edu/utfpr/cm/pi/daos/DaoClienteJuridico.java
@@ -4,10 +4,31 @@
  */
 package br.edu.utfpr.cm.pi.daos;
 
+import br.edu.utfpr.cm.pi.entidades.ClienteJuridico;
+import java.util.List;
+import org.hibernate.Query;
+
 /**
  *
  * @author TAYLY
  */
-public class DaoClienteJuridico {
+public class DaoClienteJuridico extends DaoGenerics<ClienteJuridico>{
+    
+     public DaoClienteJuridico() {
+        super.alvo = ClienteJuridico.class;
+    }
+
+    public List<ClienteJuridico> obterRazao(String razao) {
+        List<ClienteJuridico> lista = null;
+        if (razao != null || !"".equals(razao)) {
+
+            Query query = session.createQuery("From "
+                    + alvo.getSimpleName()
+                    + " where razaoSocial LIKE '"
+                    + razao + "%'");
+            lista = query.list();
+        }
+        return lista;
+    }
     
 }
