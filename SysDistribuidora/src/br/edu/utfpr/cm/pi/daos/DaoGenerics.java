67bb8c78d7399ece57b02dc37d512c5ed2436cd1
Tayly <usaf_tayly@hotmail.com>
2013-09-09 22:32:02 -0300

    Implementação DaoGenerics

--- SysDistribuidora/src/br/edu/utfpr/cm/pi/daos/DaoGenerics.java
+++ SysDistribuidora/src/br/edu/utfpr/cm/pi/daos/DaoGenerics.java
@@ -11,6 +11,7 @@ import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Query;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
@@ -30,7 +31,99 @@ import org.hibernate.Transaction;
     public void persist(T o) {
         session.saveOrUpdate(o);
         session.flush();
+    
+     try {
+
+            session = TransactionUtil.obterSessao();
+            transaction = TransactionUtil.iniciarTransacao(session);
+            session.saveOrUpdate(o);
+            transaction.commit();
+
+        } catch (HibernateException e) {
+
+            transaction.rollback();
+            Util.dispayMsg("Ocorreu um erro ao persistir o objeto.\n" + e.fillInStackTrace());
+//            System.err.println("Ocorreu um erro ao persistir o objeto.\n" + e.fillInStackTrace());
+        } finally {
+
+            TransactionUtil.fecharSessao(session);
+        }
+    
+     @Override
+    public void remover(T o) {
+//        session.delete(o);
+//        session.flush();
+        try {
+
+            session = TransactionUtil.obterSessao();
+            transaction = TransactionUtil.iniciarTransacao(session);
+            session.delete(o);
+            transaction.commit();
+
+        } catch (HibernateException e) {
+
+            transaction.rollback();
+            Util.dispayMsg("Ocorreu um erro ao remover o objeto.\n" + e.fillInStackTrace());
+//            System.err.println("Ocorreu um erro ao persistir o objeto.\n" + e.fillInStackTrace());
+        } finally {
+
+            TransactionUtil.fecharSessao(session);
+        }
     }
+
+    @Override
+    public T obterPorId(int id) {
+
+        org.hibernate.Query select = null;
+        T objeto = null;
+        try {
+
+            session = TransactionUtil.obterSessao();
+            transaction = TransactionUtil.iniciarTransacao(session);
+            if (id > 0) {
+
+                select = session.createQuery("From " + alvo.getSimpleName() + " where id = " + id);
+                objeto = (T) select.uniqueResult();
+
+            }
+        } catch (Exception e) {
+
+            transaction.rollback();
+            e.printStackTrace();
+        } finally {
+
+            session.close();
+            return objeto;
+        }
+    }
+
+    @Override
+    public List<T> listar(String filtro) {
+
+        List<T> lista = null;
+        org.hibernate.Query query = null;
+
+        try {
+
+            session = TransactionUtil.obterSessao();
+            transaction = TransactionUtil.iniciarTransacao(session);
+            if (filtro != null) {
+                query = session.createQuery("FROM " + alvo.getSimpleName() + " WHERE nome LIKE '%" + filtro + "%'");
+                lista = query.list();
+            }
+        } catch (Exception e) {
+            transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+            return lista;
+        }
+    }
+}
+    
+   
+    }
+   
 
     
     
