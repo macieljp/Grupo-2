6be2ac72b9f7730d8aa8831d77c1d7aca77b1a9b
Tayly <usaf_tayly@hotmail.com>
2013-09-29 21:05:50 -0300

    #12 Criação das  Entidades  Estado/Cidade

---
+++ SysDistribuidora/src/br/edu/utfpr/cm/pi/daos/DaoCidade.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.utfpr.cm.pi.daos;
+
+import br.edu.utfpr.cm.pi.entidades.Cidade;
+
+/**
+ *
+ * @author paulo
+ */
+public class DaoCidade extends DaoGenerics<Cidade>{
+    public DaoCidade() {
+        super(Cidade.class);
+    }
+}
