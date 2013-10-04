ea130c37fd59bf22aad48755143f09c51876769d
Tayly <usaf_tayly@hotmail.com>
2013-10-04 11:56:10 -0300

    #21 Criação Entidade Ordem de Venda

--- SysDistribuidora/src/br/edu/utfpr/cm/pi/entidades/OrdemVenda.java
+++ SysDistribuidora/src/br/edu/utfpr/cm/pi/entidades/OrdemVenda.java
@@ -6,6 +6,7 @@ package br.edu.utfpr.cm.pi.entidades;
 
 import java.io.Serializable;
 import java.util.Calendar;
+import java.util.List;
 import javax.persistence.Basic;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -14,7 +15,9 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import org.hibernate.annotations.Cascade;
@@ -31,147 +34,18 @@ public class OrdemVenda implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     
+    @OneToOne
+    private Cliente cliente;
+    
+    
+    @ManyToMany
+    private List<Produto> produtosVenda;
+    
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Calendar dataVenda;
+    
     @Temporal(javax.persistence.TemporalType.DATE)
     private Calendar dataVencimento;
-    @ManyToOne(fetch = FetchType.EAGER)
-    private Pessoa cliente;
-    @ManyToOne(fetch = FetchType.EAGER)
-    private Funcionario funcionario;
-    @Basic
-    @Column(length = 200)
-    private String item1;
-    @Basic
-    @Column(length = 200)
-    private String item2;
-    @Basic
-    @Column(length = 200)
-    private String item3;
-    @Basic
-    @Column(length = 200)
-    private String item4;
-    private Double valor1;
-    private Double valor2;
-    private Double valor3;
-    private Double valor4;
-    private Double valorTotal;
-    private String status;
-    private Integer parcelas;
-    private Integer parcelasRestantes;
-    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    @Cascade(org.hibernate.annotations.CascadeType.REMOVE)
-    private Endereco endereco;
-    private String banco;
-    private String tipoCheque;
-    private double juros;
-    private String valorPorExtenso;
-    
-    private String nomeDoCheque;
-    private String cnpjDoCheque;
-    private String valorDoCheque;
-    private String dataCheque;
-    private String agenciaCheque;
-    private String contaCheque;
-    
-    public String getNomeDoCheque() {
-        return nomeDoCheque;
-    }
-
-    public void setNomeDoCheque(String nomeDoCheque) {
-        this.nomeDoCheque = nomeDoCheque;
-    }
-
-    public String getCnpjDoCheque() {
-        return cnpjDoCheque;
-    }
-
-    public void setCnpjDoCheque(String cnpjDoCheque) {
-        this.cnpjDoCheque = cnpjDoCheque;
-    }
-
-    public String getValorDoCheque() {
-        return valorDoCheque;
-    }
-
-    public void setValorDoCheque(String valorDoCheque) {
-        this.valorDoCheque = valorDoCheque;
-    }
-
-    public String getDataCheque() {
-        return dataCheque;
-    }
-
-    public void setDataCheque(String dataCheque) {
-        this.dataCheque = dataCheque;
-    }
-
-    public String getAgenciaCheque() {
-        return agenciaCheque;
-    }
-
-    public void setAgenciaCheque(String agenciaCheque) {
-        this.agenciaCheque = agenciaCheque;
-    }
-
-    public String getContaCheque() {
-        return contaCheque;
-    }
-
-    public void setContaCheque(String contaCheque) {
-        this.contaCheque = contaCheque;
-    }
-    
-    
-
-    public OrdemVenda() {
-    }
-
-    public double getJuros() {
-        return juros;
-    }
-
-    public void setJuros(double juros) {
-        this.juros = juros;
-    }
-
-    public String getValorPorExtenso() {
-        return valorPorExtenso;
-    }
-
-    public void setValorPorExtenso(String valorPorExtenso) {
-        this.valorPorExtenso = valorPorExtenso;
-    }
-
-    public String getBanco() {
-        return banco;
-    }
-
-    public void setBanco(String banco) {
-        this.banco = banco;
-    }
-
-    public String getTipoCheque() {
-        return tipoCheque;
-    }
-
-    public void setTipoCheque(String tipoCheque) {
-        this.tipoCheque = tipoCheque;
-    }
-
-    public Funcionario getFuncionario() {
-        return funcionario;
-    }
-
-    public void setFuncionario(Funcionario funcionario) {
-        this.funcionario = funcionario;
-    }
-
-    public Integer getParcelasRestantes() {
-        return parcelasRestantes;
-    }
-
-    public void setParcelasRestantes(Integer parcelasRestantes) {
-        this.parcelasRestantes = parcelasRestantes;
-    }
 
     public Integer getId() {
         return id;
@@ -181,7 +55,29 @@ public class OrdemVenda implements Serializable {
         this.id = id;
     }
 
- 
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtosVenda;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtosVenda = produtos;
+    }
+
+    public Calendar getDataVenda() {
+        return dataVenda;
+    }
+
+    public void setDataVenda(Calendar dataVenda) {
+        this.dataVenda = dataVenda;
+    }
 
     public Calendar getDataVencimento() {
         return dataVencimento;
@@ -190,117 +86,8 @@ public class OrdemVenda implements Serializable {
     public void setDataVencimento(Calendar dataVencimento) {
         this.dataVencimento = dataVencimento;
     }
-
-    public Pessoa getCliente() {
-        return cliente;
-    }
-
-    public void setCliente(Pessoa cliente) {
-        this.cliente = cliente;
-    }
-
-   
-
-    public Double getValorTotal() {
-        return valorTotal;
-    }
-
-    public void setValorTotal(Double valor) {
-        this.valorTotal = valor;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public Integer getParcelas() {
-        return parcelas;
-    }
-
-    public void setParcelas(Integer parcelas) {
-        this.parcelas = parcelas;
-    }
-
-    public Endereco getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(Endereco endereco) {
-        this.endereco = endereco;
-    }
-
-    public String getItem1() {
-        return item1;
-    }
-
-    public void setItem1(String item1) {
-        this.item1 = item1;
-    }
-
-    public String getItem2() {
-        return item2;
-    }
-
-    public void setItem2(String item2) {
-        this.item2 = item2;
-    }
-
-    public String getItem3() {
-        return item3;
-    }
-
-    public void setItem3(String item3) {
-        this.item3 = item3;
-    }
-
-    public String getItem4() {
-        return item4;
-    }
-
-    public void setItem4(String item4) {
-        this.item4 = item4;
-    }
-
-    public Double getValor1() {
-        return valor1;
-    }
-
-    public void setValor1(Double valor1) {
-        this.valor1 = valor1;
-    }
-
-    public Double getValor2() {
-        return valor2;
-    }
-
-    public void setValor2(Double valor2) {
-        this.valor2 = valor2;
-    }
-
-    public Double getValor3() {
-        return valor3;
-    }
-
-    public void setValor3(Double valor3) {
-        this.valor3 = valor3;
-    }
-
-    public Double getValor4() {
-        return valor4;
-    }
-
-    public void setValor4(Double valor4) {
-        this.valor4 = valor4;
-    }
-
-    @Override
-    public String toString() {
-        return "OrdemVenda{" + "id=" + id + ", dataVencimento=" + dataVencimento + ", cliente=" + cliente + ", funcionario=" + funcionario + ", item1=" + item1 + ", item2=" + item2 + ", item3=" + item3 + ", item4=" + item4 + ", valor1=" + valor1 + ", valor2=" + valor2 + ", valor3=" + valor3 + ", valor4=" + valor4 + ", valorTotal=" + valorTotal + ", status=" + status + ", parcelas=" + parcelas + ", parcelasRestantes=" + parcelasRestantes + ", endereco=" + endereco + ", banco=" + banco + ", tipoCheque=" + tipoCheque + ", juros=" + juros + ", valorPorExtenso=" + valorPorExtenso + '}';
-    }
+    
+    
 
  
 }
